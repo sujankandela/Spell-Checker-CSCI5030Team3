0f97e84b04af4d156233f9ee3a44509bb910e80d
HalimaMalik <89631119+HalimaMalik@users.noreply.github.com>
2021-11-17 01:46:41 -0600

    Modified edit and spellcheck
    Modified edit distance of a word and and the spellcheck function

--- src/team03.py
+++ src/team03.py
@@ -4,42 +4,41 @@ from nltk.tokenize import word_tokenize
 import re
 from textblob import TextBlob
 
-file = open("frequent_english_3500.txt")
-term_list = {}
+file = open("frequent_english_18000.txt")
+english_term_list = {}
 for line in file:
     key, value = line.split()
-    term_list[key] = value
+    english_term_list[key] = value
     
 def detect_language(text):
     text_language = TextBlob(text)
     language = text_language.detect_language()
     return language
     
-def edit_distance1(word):
-    suggestions = []
-    for key,value in term_list.items():
-        edit = nltk.edit_distance(word, key)
-        if (edit == 1):
-            suggestions.append(key)
-    print("mispelled words are:", word)
-    print("possible corrections are:", suggestions)
-        
-def edit_distance2(word):
-   suggestions = []
-   for key,value in term_list.items():
-       edit = nltk.edit_distance(word, key)
-       if (edit == 1 or edit == 2):
-            suggestions.append(key)
-   print("mispelled words are:", word)
-   print("possible corrections are:", suggestions)
-   
+def english_edit_distance(word):
+    english_suggestions = []
+    if word not in english_term_list:
+        if (len(word)<=4):
+            for key,value in english_term_list.items():
+                edit = nltk.edit_distance(word, key)
+                if (edit == 1):
+                    english_suggestions.append(key)
+            print("mispelled words are:", word)
+            print(english_suggestions)
+        else:
+            if (5<=len(word)<=12):
+                for key,value in english_term_list.items():
+                    if word not in english_term_list:
+                        edit = nltk.edit_distance(word, key)
+                        if (edit == 1 or edit == 2):
+                            english_suggestions.append(key)
+                print("mispelled words are:", word)
+                print(english_suggestions)  
+    
 def spellCheck(text):
-    words = text.split()
     language = detect_language(text)
-    for i in words:
-        if i not in term_list:
-            word_length = len(i)
-            if (word_length <= 4):
-                edit_distance1(i)
-            else:
-                edit_distance2(i)
+    print(language)
+    words = text.split()
+    if (language != 'ga'):
+        for i in words:
+            english_edit_distance(i)
