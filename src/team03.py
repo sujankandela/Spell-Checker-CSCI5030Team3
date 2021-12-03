a191671645efcfc02babbef7bf94be06e546feae
rajrajput79 <89624908+rajrajput79@users.noreply.github.com>
2021-12-03 14:31:50 -0600

    Getting language from Front End
    User can select language from front end in which he wishes to write the text

--- src/team03.py
+++ src/team03.py
@@ -1,29 +1,34 @@
 from collections import Counter
-import nltk
+import nltk;
 from nltk.tokenize import word_tokenize
+import os
 import re
+
 from textblob import TextBlob
 
-def irish_file_read():
-    file = open("irish_corpus.txt", encoding="utf8").read()
+from flask import Flask, redirect, url_for, render_template, request
+app = Flask(__name__)
+
+def irish_file_read(): # Irish File Reading
+    file = open("frequent_irish_3500.txt", encoding="utf8").read() # Edit Filename
     tokens = word_tokenize(file)
     tokens_words = [word for word in tokens if word.isalnum()]
     vocab = Counter(tokens_words)
     return vocab
 
-def english_file_read():
-    file = open("english_corpus.txt", encoding="utf8").read()
+def english_file_read(): # English File Reading
+    file = open("frequent_english_3000.txt", encoding="utf8").read() # Edit Filename
     tokens = word_tokenize(file)
     tokens_words = [word for word in tokens if word.isalnum()]
     vocab = Counter(tokens_words)
     return vocab
     
-def detect_language(text):
+def detect_language(text): # Detecting Language on backend
     text_language = TextBlob(text)
     language = text_language.detect_language()
     return language
     
-def english_word_suggestions(word):
+def english_word_suggestions(word): # Main function to give suggestions for English
     english_term_list = english_file_read()
     english_suggestions = []
     if (word.isalnum() == False):
@@ -43,7 +48,7 @@ def english_word_suggestions(word):
                         english_suggestions.append(key)              
                 return english_suggestions 
                 
-def irish_word_suggestions(word):
+def irish_word_suggestions(word): # Main function to give suggestions for Irish
     irish_term_list = irish_file_read()
     irish_suggestions = []
     if (word.isalnum() == False):
@@ -63,12 +68,34 @@ def irish_word_suggestions(word):
                         irish_suggestions.append(key)              
                 return irish_suggestions
     
-def spellCheck(text):
-    language = detect_language(text)
+def spellCheck(text,languagetowrite): # Main function
+    suggestions=[]
+    language = languagetowrite
     words = text.split()
-    if (language != 'ga'):
+    if (language == "English"):
         for i in words:
-            return english_word_suggestions(i)
+            e=english_word_suggestions(i)
+            suggestions.append(e) # appending suggestion if there are more than a single word 
+        return suggestions
     else:
         for i in words:
-            return irish_word_suggestions(i)
+            e=irish_word_suggestions(i)
+            suggestions.append(e) # appending suggestion if there are more than a single word 
+        return suggestions
+
+@app.route('/', methods=['POST'])
+def check():
+    if request.method == 'POST':
+        p=request.form.getlist('word') # getting wordlist from front end
+        languagetowrite=request.form.get('language')
+        textoutput = spellCheck(p[0],languagetowrite)# send wordlist to function
+        listToStr = ' '.join([str(element) for element in textoutput])
+        return listToStr
+    
+
+@app.route('/')
+def index():
+    return render_template("team03.html")
+
+if __name__ == '__main__':
+    app.run()
