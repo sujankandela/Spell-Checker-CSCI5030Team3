d7861a4a2906d3b66f95bb70d07e14cfe923e3cb
Sujan Kandela <sujankandela999@gmail.com>
2021-12-08 02:45:50 -0600

    Revert "Final python: Working file"

--- src/team03.py
+++ src/team03.py
@@ -73,6 +73,20 @@ def spellCheck(text,languagetowrite): # Main function
     language = languagetowrite
     words = text.split()
     if (language == "English"):
+            print(i)
+            word= i + ":"
+            e=english_word_suggestions(i)
+            print("e")
+            print(e)
+            x= ','.join([str(element) for element in e])
+            print("x")
+            print(x)
+            word=word + x + "\n"
+            suggestions.append(word)
+            # appending suggestion if there are more than a single word 
+            print(suggestions)
+            print("suggestions")
+    else:
         for i in words:
             print(i)
             word= i + ":"
@@ -87,34 +101,14 @@ def spellCheck(text,languagetowrite): # Main function
             # appending suggestion if there are more than a single word 
             print(suggestions)
             print("suggestions")
-        return suggestions
-    else:
-        for i in words:
-            print(i)
-            word= i + ":"
-            e=irish_word_suggestions(i)
-            print("e")
-            print(e)
-            x= ','.join([str(element) for element in e])
-            print("x")
-            print(x)
-            word=word + x + "\n"
-            suggestions.append(word)
-            # appending suggestion if there are more than a single word 
-            print(suggestions)
-            print("suggestions")
-        return suggestions
 
 @app.route('/', methods=['POST'])
 def check():
     if request.method == 'POST':
         p=request.form.getlist('word') # getting wordlist from front end
-        print(p[0])
         languagetowrite=request.form.get('language')
         textoutput = spellCheck(p[0],languagetowrite)# send wordlist to function
-        print(textoutput)
-        listToStr = ''.join([str(element) for element in textoutput])
-        print(listToStr)
+        listToStr = ' '.join([str(element) for element in textoutput])
         return listToStr
     
 
